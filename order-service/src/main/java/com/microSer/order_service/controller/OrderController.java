5a83191bab93c0c1693c4ddb674146a663a1fe63
Kavindulakmal <avakavindu@gmail.com>
2024-08-05 00:49:47 +0530

    first Commit

---
+++ order-service/src/main/java/com/microSer/order_service/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.microSer.order_service.controller;
+
+import com.microSer.order_service.dto.OrderRequest;
+import com.microSer.order_service.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+}
+
